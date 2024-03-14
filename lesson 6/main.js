bcd060edd80619f9a8f1d1adb1d247d05c53f4e0
zafarbek2005 <153996622+zafarbek2005@users.noreply.github.com>
2024-03-14 15:56:17 +0500

    .

--- lesson 6/main.js
+++ lesson 6/main.js
@@ -1,6 +1,10 @@
 const sidebar = document.querySelector(".sidebar");
 const sidebarBtn = document.querySelector(".sidebar-btn");
+const bt = document.querySelector(".bt");
 
 sidebarBtn.addEventListener("click", () => {
     sidebar.classList.toggle("active");
 });
+bt.addEventListener("click", () => {
+    sidebar.classList.toggle("active");
+})
